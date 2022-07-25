e0a3a43c0e36c63a5dfe47ed94b7470ea316d12e
Zaoyad Khan Raad <72065950+raad9114@users.noreply.github.com>
2022-07-25 14:47:29 +0600

    Update app.config.js
    Use own Key

--- config/app.config.js
+++ config/app.config.js
@@ -1,6 +1,6 @@
 const ONE_SIGNAL_CONFIG = {
-    APP_ID: "4f0d1817-9115-4d70-bc32-937ee40a7b23",
-    API_KEY: "ZDhlNDYzZjYtYmFkMy00NWE5LTkyYzgtNWFkNDNlZTJjM2Jj"
+    APP_ID: "",
+    API_KEY: ""
 };
 module.exports = {
     ONE_SIGNAL_CONFIG,
