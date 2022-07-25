cce4d8235a091f5c45a302e645c67f955860e2ce
Zaoyad Khan Raad <raad9114@gmail.com>
2022-07-25 14:42:03 +0600

    Push notification API

---
+++ config/app.config.js
@@ -0,0 +1,7 @@
+const ONE_SIGNAL_CONFIG = {
+    APP_ID: "4f0d1817-9115-4d70-bc32-937ee40a7b23",
+    API_KEY: "ZDhlNDYzZjYtYmFkMy00NWE5LTkyYzgtNWFkNDNlZTJjM2Jj"
+};
+module.exports = {
+    ONE_SIGNAL_CONFIG,
+};
